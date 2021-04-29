37282bdc777d780ce1fb6802318b08fc9cf96352
Arthur Barski <barski.10877716@gmail.com>
2021-04-29 20:05:28 +0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-const info = document.querySelector(".info")
-const infoPic = document.querySelector(".info__content-pic")
-const infoTextLeft = document.querySelector(".info__text_left")
-const infoTextRight = document.querySelector(".info__text_right")
+const info = document.querySelector(".js-info")
+const infoPic = document.querySelector(".js-pic")
+const infoTextLeft = document.querySelector(".js-text-left")
+const infoTextRight = document.querySelector(".js-text-right")
 
 
 const options = {
@@ -10,26 +10,51 @@ const options = {
     threshold: 0
 }
 
+const state = {
+    lastScrollPosition: 800
+}
+
 const observer = new IntersectionObserver(function (entries, obserwer) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             window.addEventListener("scroll", function () {
-                if (pageYOffset > 800) {
-                    info.classList.add("info_active")
-                    infoPic.style.transform = "translate(0px, 0px) rotate(0deg)"
-                    document.body.style.overflow = "hidden"
-                    // setTimeout(function () {
-                    //     document.body.style.overflow = "auto"
-                    // }, 3000)
-                    // if (pageYOffset > 1000) {
-                    //     infoPic.style.transform = "translate(-200px, -780px) rotate(-20deg)"
-                    // }
-                } else {
-                    info.classList.remove("info_active")
-                    infoPic.style.transform = `translate(${-100 + pageYOffset / 10 + 20}px, ${800 - pageYOffset / 1.1 - 72.7}px) rotate(${20 - pageYOffset / 45 - 2}deg)`
-                    infoTextLeft.style.transform = `translateX(${-100 + pageYOffset / 10}%)`
-                    infoTextRight.style.transform = `translateX(${100 - pageYOffset / 10}%)`
+                let currentScroll = window.pageYOffset;
+                info.classList.remove("info_active")
+                infoPic.style.transform = `translate(${-100 + pageYOffset / 10 + 20}px, ${800 - pageYOffset / 1.1 - 72.7}px) rotate(${20 - pageYOffset / 45 - 2}deg)`
+                infoTextLeft.style.transform = `translateX(${-100 + pageYOffset / 10}%)`
+                infoTextRight.style.transform = `translateX(${100 - pageYOffset / 10}%)`
+                // Вниз
+                if (currentScroll > state.lastScrollPosition) {
+                    if (currentScroll < 1000 && currentScroll > 800) {
+                        info.classList.add("info_active")
+                        infoPic.style.transform = "translate(0px, 0px) rotate(0deg)"
+                    } else {
+
+                    }
                 }
+                // Вверх
+                if (currentScroll <= state.lastScrollPosition) {
+
+                }
+
+
+
+                // if (pageYOffset > 800) {
+                //     info.classList.add("info_active")
+                //     infoPic.style.transform = "translate(0px, 0px) rotate(0deg)"
+                //     // document.body.style.overflow = "hidden"
+                //     // setTimeout(function () {
+                //     //     document.body.style.overflow = "auto"
+                //     // }, 3000)
+                //     // if (pageYOffset > 1000) {
+                //     //     infoPic.style.transform = "translate(-200px, -780px) rotate(-20deg)"
+                //     // }
+                // } else {
+                //     info.classList.remove("info_active")
+                //     infoPic.style.transform = `translate(${-100 + pageYOffset / 10 + 20}px, ${800 - pageYOffset / 1.1 - 72.7}px) rotate(${20 - pageYOffset / 45 - 2}deg)`
+                //     infoTextLeft.style.transform = `translateX(${-100 + pageYOffset / 10}%)`
+                //     infoTextRight.style.transform = `translateX(${100 - pageYOffset / 10}%)`
+                // }
             })
             // info.addEventListener("scroll", function (e) {
             //     if (e.detail == 'down') {
